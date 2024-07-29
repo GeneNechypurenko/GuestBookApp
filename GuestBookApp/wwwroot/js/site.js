925e982b95010ab4572c2062224fa11a5f082205
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-07-29 14:44:06 +0300

    upd

--- GuestBookApp/wwwroot/js/site.js
+++ GuestBookApp/wwwroot/js/site.js
@@ -1,39 +1,29 @@
 ﻿$(document).ready(function () {
-    $('#guestBookForm').submit(function (event) {
+    // Обработчик клика для открытия модального окна входа
+    $('#loginLink').on('click', function (event) {
         event.preventDefault();
+        $('#loginModal').modal('show');
+    });
+
+    // Обработчик клика для открытия модального окна регистрации
+    $('#registerLink').on('click', function (event) {
+        event.preventDefault();
+        $('#registerModal').modal('show');
+    });
+
+    // Существующий код для отправки форм
+    $('#loginForm').submit(function (event) {
+        event.preventDefault(); // Prevent the default form submission
         var form = $(this);
         $.ajax({
             type: form.attr('method'),
             url: form.attr('action'),
             data: form.serialize(),
             success: function (response) {
-                $('#entries').html(response);
-                form[0].reset();
+                window.location.href = '/Home/Index'; // Redirect to home page
             },
             error: function (response) {
-                var errors = response.responseJSON;
-                form.find('.text-danger').text('');
-                for (var key in errors) {
-                    var errorContainer = form.find('[asp-validation-for="' + key + '"]');
-                    if (errorContainer.length > 0) {
-                        errorContainer.text(errors[key][0]);
-                    }
-                }
-            }
-        });
-    });
-
-    $('#loginForm').submit(function (event) {
-        event.preventDefault();
-        var form = $(this);
-        $.ajax({
-            type: form.attr('method'),
-            url: form.attr('action'),
-            data: form.serialize(),
-            success: function () {
-                location.reload();
-            },
-            error: function (response) {
+                // Handle validation errors
                 var errors = response.responseJSON;
                 form.find('.text-danger').text('');
                 for (var key in errors) {
@@ -47,16 +37,17 @@
     });
 
     $('#registerForm').submit(function (event) {
-        event.preventDefault();
+        event.preventDefault(); // Prevent the default form submission
         var form = $(this);
         $.ajax({
             type: form.attr('method'),
             url: form.attr('action'),
             data: form.serialize(),
-            success: function () {
-                location.reload();
+            success: function (response) {
+                window.location.href = '/Account/Login'; // Redirect to login page
             },
             error: function (response) {
+                // Handle validation errors
                 var errors = response.responseJSON;
                 form.find('.text-danger').text('');
                 for (var key in errors) {
@@ -67,15 +58,5 @@
                 }
             }
         });
-    });
-
-    $('#logoutBtn').click(function () {
-        $.post('/Account/Logout', function () {
-            location.reload();
-        });
-    });
-
-    $.get('/Home/GetMessages', function (response) {
-        $('#entries').html(response);
     });
 });
