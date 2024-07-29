da1f8ab26da78dc3446d15c21726b173b222ce33
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-07-29 18:43:23 +0300

    complete

--- GuestBookApp/Controllers/AccountController.cs
+++ GuestBookApp/Controllers/AccountController.cs
@@ -5,51 +5,53 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GuestBookApp.Controllers
 {
-    public class AccountController : Controller
-    {
-        private readonly IUserRepository _userRepository;
+	public class AccountController : Controller
+	{
+		private readonly IUserRepository _userRepository;
 
-        public AccountController(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-        }
+		public AccountController(IUserRepository userRepository)
+		{
+			_userRepository = userRepository;
+		}
 
-        [HttpPost]
-        public IActionResult Login(LoginViewModel model)
-        {
-            HttpContext.Session.SetString("Username", model.Username);
-            return Ok();
-        }
+		[HttpPost]
+		public IActionResult Login(LoginViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				HttpContext.Session.SetString("Username", model.Username);
+				return Ok();
+			}
+			return BadRequest(ModelState);
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var existingUser = await _userRepository.GetUserByUsernameAsync(model.Username);
-                if (existingUser == null)
-                {
-                    var user = new User
-                    {
-                        Username = model.Username,
-                        PasswordHash = BCrypt.Net.BCrypt.HashPassword(model.Password)
-                    };
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var existingUser = await _userRepository.GetUserByUsernameAsync(model.Username);
+				if (existingUser == null)
+				{
+					var user = new User
+					{
+						Username = model.Username,
+						PasswordHash = BCrypt.Net.BCrypt.HashPassword(model.Password)
+					};
 
-                    await _userRepository.AddUserAsync(user);
-                    return Ok();
-                }
+					await _userRepository.AddUserAsync(user);
+					return Ok();
+				}
+			}
 
-                ModelState.AddModelError("", "Username already exists.");
-            }
+			return BadRequest(ModelState);
+		}
 
-            return BadRequest(ModelState);
-        }
-
-        [HttpPost]
-        public IActionResult Logout()
-        {
-            HttpContext.Session.Clear();
-            return Ok();
-        }
-    }
+		[HttpPost]
+		public IActionResult Logout()
+		{
+			HttpContext.Session.Clear();
+			return Ok();
+		}
+	}
 }
